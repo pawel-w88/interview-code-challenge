6aa3c278082facf969a5658f201c64b0a79ae5b3
Pawel-W88 <rws.pawlak@gmail.com>
2025-01-13 09:37:41 +0100

    fix sorted squerwd array

--- sortedSqueredArray.js
+++ sortedSqueredArray.js
@@ -1,5 +1,5 @@
 const sortedSqueredArray = (array) => {
-    let squeredArray = array.map((el) => el ** 2).sort((a, b) => {a - b})
+    let squeredArray = array.map((el) => el ** 2).sort((a, b) => a - b)
     return squeredArray
 } 
 console.log(sortedSqueredArray([-4, -1, 0, 3, 10])) 
